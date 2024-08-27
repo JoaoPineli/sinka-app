ae53e3ed6f01066ba7677c9d1763e2715a697df0
dextryan <jgpineli@gmail.com>
2024-08-27 02:41:52 -0300

    fixed type issues

--- src/utils/dataToCsv.ts
+++ src/utils/dataToCsv.ts
@@ -1,19 +1,25 @@
-export function dataToCsv(headers, data, filename: string) {
-    const csv = [
+interface DataRow {
+    [key: string]: string;
+}
+
+export function dataToCsv(headers: string[], data: DataRow[], filename: string): void {
+    const csv: string = [
         headers.join(','),
-        ...data.map(row => headers.map(header => { 
-            const value = row[String(header).toLowerCase()];
-            if(header === 'Nascimento') {
-                const dateParts = value.split('T')[0].split('-');
-                return `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`;
-            }
-            return value;
-        }).join(','))
-    ].join('\n');
-    const csvWithBom = '\uFEFF' + csv; // BOM para forçar o Excel a abrir o arquivo com a codificação correta
-    const blob = new Blob([csvWithBom], {type: 'text/csv'});
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
+        ...data.map((row: DataRow) =>
+            headers.map((header: string) => {
+                const value: string = row[String(header).toLowerCase()];
+                if (header === 'Nascimento') {
+                    const dateParts: string[] = value.split('T')[0].split('-');
+                    return `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`;
+                }
+                return value;
+            }).join(',')
+        )].join('\n');
+
+    const csvWithBom: string = '\uFEFF' + csv; // BOM para forçar o Excel a abrir o arquivo com a codificação correta
+    const blob: Blob = new Blob([csvWithBom], { type: 'text/csv' });
+    const url: string = window.URL.createObjectURL(blob);
+    const a: HTMLAnchorElement = document.createElement('a');
     a.href = url;
     a.download = filename;
     a.click();
